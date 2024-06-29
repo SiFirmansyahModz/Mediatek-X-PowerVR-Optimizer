6b2c9bf08648d636cadb980b61abdf54727928c6
SiFirmansyahModz <164661353+SiFirmansyahModz@users.noreply.github.com>
2024-06-29 16:33:17 +0700

    exe.sh

--- exe.sh
+++ exe.sh
@@ -1,4 +1,4 @@
 rm -rf /data/local/tmp/MTKPVR
-cp /sdcard/bin/MTKPVR /data/local/tmp 
+cp /sdcard/Mediatek-X-PowerVR-Optimizer/bin/MTKPVR /data/local/tmp 
 chmod +x /data/local/tmp/MTKPVR
 /data/local/tmp/MTKPVR
